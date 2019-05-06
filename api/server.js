a955a6725a31d94a787bfd25b0fd7217c0dd835d
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-06 13:31:10 -0500

    MVP

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan')
+const cors = require('cors');
+const authRouter = require('./router/authRouter');
+const dbConfig = require('./database/dbConfig.js')
+const server = express();
+
+
+
+server.use(helmet());
+server.use(morgan('dev'))
+server.use(express.json());
+server.use(cors());
+
+
+
+server.use('/api/auth', authRouter);
+
+
+
+
+module.exports = server;
